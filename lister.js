e1e87658ccbf02a59d619541dc7171f300c69284
Jack Zhang <15jzhang@gmail.com>
2017-07-08 17:04:23 -0700

    Implement filtering

--- lister.js
+++ lister.js
@@ -3,6 +3,28 @@ var lister = new Vue({
   data: {
     input: '',
     items: [
-    ]
+    ],
+    filter: 'all'
+  },
+  computed: {
+    showControl: function () {
+      return this.items.length !== 0
+    },
+    filteredItems: function () {
+      switch (this.filter) {
+        case 'all':
+          return this.items
+        case 'active':
+          return this.items.filter(function (item) {
+            return item.isActive
+          })
+        case 'complete':
+          return this.items.filter(function (item) {
+            return !item.isActive
+          })
+        default:
+          return this.items
+      }
+    }
   }
 })
