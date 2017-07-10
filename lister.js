d0e0b40066ca7f03d8a6ffc0895a680b8a1e6cd6
Jack Zhang <15jzhang@gmail.com>
2017-07-10 03:04:31 -0700

    Implement items

--- lister.js
+++ lister.js
@@ -3,8 +3,8 @@ var lister = new Vue({
   data: {
     input: '',
     items: [
-      { id: 0, isActive: true, text: 'Buy milk.' },
-      { id: 1, isActive: false, text: 'Learn Vue.' }
+      { id: 0, isActive: true, isEditing: false, editedText: '', text: 'Buy milk.' },
+      { id: 1, isActive: true, isEditing: false, editedText: '', text: 'Learn Vue.' }
     ],
     filter: 'all'
   },
@@ -26,6 +26,65 @@ var lister = new Vue({
           })
         default:
           return this.items
+      }
+    }
+  },
+  methods: {
+    addItem: function () {
+      this.items.push({
+        id: this.items.reduce(function (maxId, item) {
+          return Math.max(item.id, maxId)
+        }, -1) + 1,
+        isActive: true,
+        isEditing: false,
+        editedText: '',
+        text: this.input
+      })
+
+      this.input = ''
+    },
+    removeItem: function (id) {
+      this.items = this.items.filter(function (item) {
+        return item.id !== id
+      })
+    },
+    checkItem: function (id) {
+      console.log('tri')
+      this.items = this.items.map(function (item) {
+        return item.id === id ?
+          Object.assign({}, item, { isActive: !item.isActive }) :
+          item
+      })
+    },
+    editItem: function (id) {
+      this.items = this.items.map(function (item) {
+        return item.id === id ?
+          Object.assign({}, item, { isEditing: true, editedText: item.text }) :
+          item
+      })
+    },
+    completeEdit: function (id) {
+      this.items = this.items.map(function (item) {
+        return item.id === id ?
+          Object.assign({}, item, {
+            isEditing: false,
+            editedText: '',
+            text: item.editedText
+          }) : item
+      })
+    },
+    cancelEdit: function (id) {
+      this.items = this.items.map(function (item) {
+        return item.id === id ?
+          Object.assign({}, item, { isEditing: false, editedText: '' }) :
+          item
+      })
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
       }
     }
   }
